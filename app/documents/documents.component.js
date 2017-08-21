7609ffe25a6383609ca1d59bba3bf07f051eaf45
Noel Delgado <noeldelgadom@myhost.local>
2017-08-21 15:18:39 -0500

    section 4

---
+++ app/documents/documents.component.js
@@ -0,0 +1,46 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@angular/core");
+let DocumentsComponent = class DocumentsComponent {
+    constructor() {
+        this.pageTitle = "Document Dashboard";
+        this.documents = [
+            {
+                title: "My first doc",
+                description: "some description",
+                file_url: "http://google.com",
+                updated_at: "11/11/16",
+                image_url: "http://google.com"
+            },
+            {
+                title: "My second doc",
+                description: "some description",
+                file_url: "http://google.com",
+                updated_at: "11/11/16",
+                image_url: "http://google.com"
+            },
+            {
+                title: "My last doc",
+                description: "some description",
+                file_url: "http://google.com",
+                updated_at: "11/11/16",
+                image_url: "http://google.com"
+            },
+        ];
+    }
+};
+DocumentsComponent = __decorate([
+    core_1.Component({
+        moduleId: module.id,
+        selector: 'documents',
+        templateUrl: 'documents.component.html'
+    })
+], DocumentsComponent);
+exports.DocumentsComponent = DocumentsComponent;
+//# sourceMappingURL=documents.component.js.map
